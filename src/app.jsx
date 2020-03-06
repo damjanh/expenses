b6a1fefa8881299cc03dadbadc144b680bdddf7d
damjanh <damjan.hudobreznik@gmail.com>
2020-03-06 20:51:16 +0100

    Implement 404 page not found switch.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css';
 import './styles/styles.scss';
 
@@ -16,12 +16,19 @@ const AddExpensePage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        Not found 404!
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact />
             <Route path="/create" component={AddExpensePage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 

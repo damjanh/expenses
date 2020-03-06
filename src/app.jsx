44718fbba4f25937729dbd0219638f43c029d20c
damjanh <damjan.hudobreznik@gmail.com>
2020-03-06 21:15:07 +0100

    Refactor to NavLink and set style for active.

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import {
     BrowserRouter,
     Route,
     Switch,
-    Link,
+    NavLink,
 } from 'react-router-dom';
 import 'normalize.css';
 import './styles/styles.scss';
@@ -45,10 +45,10 @@ const Header = () => (
             Expenses
         </h1>
         <p>
-            <Link to="/">Dashboard</Link>
-            <Link to="/create">Create</Link>
-            <Link to="/edit">Edit</Link>
-            <Link to="/help">Help</Link>
+            <NavLink to="/" activeClassName="is-active" exact> Dashboard</NavLink>
+            <NavLink to="/create" activeClassName="is-active" exact>Create</NavLink>
+            <NavLink to="/edit" activeClassName="is-active" exact>Edit</NavLink>
+            <NavLink to="/help" activeClassName="is-active" exact>Help</NavLink>
         </p>
     </header>
 );

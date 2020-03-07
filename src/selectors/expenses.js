6455440157df41627f5067c6af67ab27e10546b1
damjanh <damjan.hudobreznik@gmail.com>
2020-03-07 21:55:56 +0100

    Implement expenses selector.

---
+++ src/selectors/expenses.js
@@ -0,0 +1,20 @@
+const getVisibleExpenses = (expenses, {
+    text,
+    sortBy,
+    startDate,
+    endDate,
+}) => expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expenses.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expenses.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+}).sort((a, b) => {
+    if (sortBy === 'date') {
+        return a.createdAt < b.createdAt ? 1 : -1;
+    }
+    return a.amount < b.amount ? 1 : -1;
+});
+
+
+export default getVisibleExpenses;

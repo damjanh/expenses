bce9494e8b8c7a4e73d13e24522f41dd95c67b5d
damjanh <damjan.hudobreznik@gmail.com>
2020-03-07 22:15:15 +0100

    Implement store in app.jsx and verify correctnes.

--- src/app.jsx
+++ src/app.jsx
@@ -4,5 +4,20 @@ import 'normalize.css';
 import './styles/styles.scss';
 
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisbleExpense from './selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visible = getVisbleExpense(state.expenses, state.filters);
+console.log(visible);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));

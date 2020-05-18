473572efafba3d71d2e5afae9251c31337835b5d
damjanh <damjan.hudobreznik@gmail.com>
2020-05-18 10:04:40 +0200

    Redirect to dashboard after adding expense.

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -5,13 +5,14 @@ import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
 const AddExpensePage = (props) => {
-    const { dispatch } = props;
+    const { dispatch, history } = props;
     return (
         <div>
             <h1>Add Expense</h1>
             <ExpenseForm
                 onSubmit={(expense) => {
                     dispatch(addExpense(expense));
+                    history.push('/');
                 }}
             />
         </div>
@@ -20,6 +21,7 @@ const AddExpensePage = (props) => {
 
 AddExpensePage.propTypes = {
     dispatch: PropTypes.func.isRequired,
+    history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
 };
 
 export default connect()(AddExpensePage);

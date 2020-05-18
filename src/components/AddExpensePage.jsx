2d3a2fa2193e907b7aca401289a576d933a6efdb
damjanh <damjan.hudobreznik@gmail.com>
2020-05-18 09:58:14 +0200

    Dispatch add expense action.

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm
-            onSubmit={(expense) => {
-                console.log(expense);
-            }}
-        />
-    </div>
-);
+const AddExpensePage = (props) => {
+    const { dispatch } = props;
+    return (
+        <div>
+            <h1>Add Expense</h1>
+            <ExpenseForm
+                onSubmit={(expense) => {
+                    dispatch(addExpense(expense));
+                }}
+            />
+        </div>
+    );
+};
 
-export default AddExpensePage;
+AddExpensePage.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(AddExpensePage);

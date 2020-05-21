dd2dc57f67aa1975dc12ff13250d23d1811f8502
damjanh <damjan.hudobreznik@gmail.com>
2020-05-21 12:34:56 +0200

    Setup Enzyme.

---
+++ src/tests/components/Header.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Header from '../../components/Header';
+
+// React test renderer example. Here for future reference.
+// test('Should render Header correctly', () => {
+//     const renderer = new ReactShallowRenderer();
+//     renderer.render(<Header />);
+//     expect(renderer.getRenderOutput()).toMatchSnapshot();
+// });
+
+test('Should render Header correctly', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+});

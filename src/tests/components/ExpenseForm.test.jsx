d2b58a95671a08a3c37370fa90132aff08a2e36f
damjanh <damjan.hudobreznik@gmail.com>
2020-05-22 09:09:16 +0200

    Finalize ExpenseForm component snapshot tests.

--- src/tests/components/ExpenseForm.test.jsx
+++ src/tests/components/ExpenseForm.test.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
 
 test('Should render ExpenseForm correctly', () => {
-    const wrapper = shallow(<ExpenseForm />);
+    const wrapper = shallow(<ExpenseForm onSubmit={() => {}} />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should render ExpenseForm with expense data correctly', () => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={() => {}} />);
     expect(wrapper).toMatchSnapshot();
 });

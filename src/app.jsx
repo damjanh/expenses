b7200e2398a38ddfef153c7d4c6fd99e2cf1850a
damjanh <damjan.hudobreznik@gmail.com>
2020-05-25 08:19:39 +0200

    Prepare webpack for production, Remove unused code in app.jsx.

--- src/app.jsx
+++ src/app.jsx
@@ -9,18 +9,12 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisbleExpense from './selectors/expenses';
 
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 3450 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 9340, createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 100830 }));
-
-const state = store.getState();
-const visible = getVisbleExpense(state.expenses, state.filters);
-console.log(visible);
 
 const jsx = (
     <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,57 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.jsx',
-    output: {
-        publicPath: '/',
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle_app.js',
-    },
-    devServer: {
-        inline: true,
-        port: 8001,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            {
-                                plugins: [
-                                    '@babel/plugin-proposal-class-properties',
-                                ],
-                            },
-                        ],
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    return {
+        entry: './src/app.jsx',
+        output: {
+            publicPath: '/',
+            path: path.join(__dirname, '/dist'),
+            filename: 'bundle_app.js',
+        },
+        devServer: {
+            inline: true,
+            port: 8001,
+            historyApiFallback: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                {
+                                    plugins: [
+                                        '@babel/plugin-proposal-class-properties',
+                                    ],
+                                },
+                            ],
+                        },
                     },
                 },
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html',
+            }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
-    ],
-    resolve: {
-        extensions: ['.js', '.jsx', '.scss', '.css'],
-    },
-    devtool: 'cheap-module-evel-source-map',
+        resolve: {
+            extensions: ['.js', '.jsx', '.scss', '.css'],
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-evel-source-map',
+    };
 };

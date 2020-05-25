43273ae92725beed43a17133a3dcf960e786c4a4
damjanh <damjan.hudobreznik@gmail.com>
2020-05-25 10:31:40 +0200

    Prepare project for heroku hosting.

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ const path = require('path');
 
 const app = express();
 const distPath = path.join(__dirname, '..', 'dist');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(distPath));
 app.get('*', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is running!');
 });

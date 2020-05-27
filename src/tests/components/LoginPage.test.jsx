934bf7a9faab15046aba26a23b96e96fe88b4b4d
damjanh <damjan.hudobreznik@gmail.com>
2020-05-27 10:15:08 +0200

    Implement test for LoginPage user interaction.

--- src/tests/components/LoginPage.test.jsx
+++ src/tests/components/LoginPage.test.jsx
@@ -6,3 +6,10 @@ test('Should render LoginPage correctly', () => {
     const wrapper = shallow(<LoginPage startLoginProp={() => {}} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('Should call startLogin when login button is clicked', () => {
+    const startLogin = jest.fn();
+    const wrapper = shallow(<LoginPage startLoginProp={startLogin} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogin).toHaveBeenCalled();
+});

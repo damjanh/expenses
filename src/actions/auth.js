e27918f647197fbac4f51bfdc571c828b64ca2f4
damjanh <damjan.hudobreznik@gmail.com>
2020-05-27 09:49:27 +0200

    Return fucntion instead of promise from startLogin action.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,5 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
-const startLogin = () => firebase.auth().signInWithPopup(googleAuthProvider);
+const startLogin = () => () => firebase.auth().signInWithPopup(googleAuthProvider);
 
 export default startLogin;

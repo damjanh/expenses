a4b9cc0e54a3d6f29e31038dd106270d8b4c6e91
damjanh <damjan.hudobreznik@gmail.com>
2020-05-27 07:34:07 +0200

    Remove tests with real firebase database.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,8 +1,4 @@
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import database from '../../firebase/firebase';
 import {
-    startAddExpense,
     addExpense,
     editExpense,
     removeExpense,
@@ -10,26 +6,6 @@ import {
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 
-const mockStore = configureStore([thunk]);
-
-beforeEach((done) => {
-    const expensesData = {};
-    expenses.forEach(({
-        id,
-        description,
-        note,
-        amount,
-        createdAt,
-    }) => {
-        expensesData[id] = {
-            description,
-            note,
-            amount,
-            createdAt,
-        };
-    });
-    database.ref('expenses').set(expensesData).then(() => done());
-});
 
 test('Should setup remove expense action object', () => {
     const action = removeExpense('123');
@@ -59,28 +35,6 @@ test('Should setup add expense action object', () => {
     });
 });
 
-test('Should add expense to database and store', (done) => {
-    const store = mockStore({});
-    const expense = {
-        description: expenses[0].description,
-        note: expenses[0].note,
-        amount: expenses[0].amount,
-        createdAt: expenses[0].createdAt,
-    };
-    store.dispatch(startAddExpense(expense)).then(() => {
-        const actions = store.getActions();
-        expect(actions[0]).toEqual({
-            type: 'ADD_EXPENSE',
-            id: expect.any(String),
-            ...expense,
-        });
-
-        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
-    }).then((snapshot) => {
-        expect(snapshot.val()).toEqual(expense);
-        done();
-    });
-});
 
 test('Should setup expense action with data', () => {
     const action = setExpenses(expenses);

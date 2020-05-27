f395ca246960ffef22d1c2c5f0927ec2ba20ea27
damjanh <damjan.hudobreznik@gmail.com>
2020-05-27 09:21:28 +0200

    Implement login page component.

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = () => (
+    <div>
+        <button type="button">
+            Login
+        </button>
+    </div>
+);
+
+export default LoginPage;
